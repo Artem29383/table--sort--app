45a5d287e493c9dfba8eaf6fdf6c0705079a60eb
Artem <artyom.aweryanow@yandex.ru>
2020-02-08 15:36:40 +0300

    Updates

---
+++ precache-manifest.e618d0456d37e6807cce576c3c9df9b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426309e37257ce85ee4a6182dcf12e7c",
+    "url": "/table--sort--app/index.html"
+  },
+  {
+    "revision": "736f5f49d1a56633c342",
+    "url": "/table--sort--app/static/css/main.f7371663.chunk.css"
+  },
+  {
+    "revision": "b7ad1fbeb8ad518ca03a",
+    "url": "/table--sort--app/static/js/2.cfb71821.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/table--sort--app/static/js/2.cfb71821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "736f5f49d1a56633c342",
+    "url": "/table--sort--app/static/js/main.7e26fd05.chunk.js"
+  },
+  {
+    "revision": "0d2db032700b2b766105",
+    "url": "/table--sort--app/static/js/runtime-main.6666a435.js"
+  }
+]);
